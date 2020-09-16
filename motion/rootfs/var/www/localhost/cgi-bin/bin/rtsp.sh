33c3cc70890ab3b39f2fdf519712bb8b74f7d47e
dcmartin <github@dcmartin.com>
2020-09-16 11:04:43 -0700

    lgtm

--- motion/rootfs/var/www/localhost/cgi-bin/bin/rtsp.sh
+++ motion/rootfs/var/www/localhost/cgi-bin/bin/rtsp.sh
@@ -6,16 +6,32 @@ if [ -d "/tmpfs" ]; then TMPDIR="/tmpfs"; else TMPDIR="/tmp"; fi
 ### MAIN
 ###
 
-PIDFILE="${TMPDIR}/${0##*/}.pid"
-if [ -e ${PIDFILE} ]; then
-  echo "IN-PROGRESS; PID: " $(cat ${PIDFILE}) &> /dev/stderr
-else
-  exec 0>&- # close stdin
-  exec 1>&- # close stdout
-  exec 2>&- # close stderr
-  ./bin/mkrtsp.sh ${1} &
-  PID=$!
-  echo "${PID}" | tee ${PIDFILE}
-  wait ${PID}
-  rm -f ${PIDFILE}
+if [ -z "${1:-}" ] || [ ! -s "${1:-}" ] || [ -z "${2:-}" ]; then
+  exit 1
 fi
+
+OUTPUT=${1}
+PIDFILE=${2}
+
+if [ -s ${PIDFILE} ]; then
+  PID=$(cat ${PIDFILE})
+  if [ ! -z "${PID:-}" ]; then
+    PS=($(ps alxwww | egrep "^[ \t]*${PID} " | awk '{ print $1 }'))
+    if [ "${PS:-}" = "${PID}" ]; then
+      exit
+    else
+      rm -f ${PIDFILE}
+    fi
+  fi
+fi
+
+
+exec 0>&- # close stdin
+exec 1>&- # close stdout
+exec 2>&- # close stderr
+
+${0%/*}/mkrtsp.sh ${OUTPUT} &
+PID=$!
+echo "${PID}" > ${PIDFILE}
+wait ${PID}
+rm -f ${PIDFILE}
