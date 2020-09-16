af01ea39f0ad249493b66d9e1e367009c23f16a1
dcmartin <github@dcmartin.com>
2020-09-16 11:04:43 -0700

    lgtm

--- motion/rootfs/var/www/localhost/cgi-bin/bin/mkrtsp.sh
+++ motion/rootfs/var/www/localhost/cgi-bin/bin/mkrtsp.sh
@@ -30,22 +30,14 @@ find_rtsp()
 ### MAIN
 ###
 
-if [ ! -z "${1}" ]; then
-  pidfile="${2:-/tmp/${0##*/}.pid}"
-  mkdir -p ${pidfile%/*}
-  if [ ! -s ${pidfile} ]; then
-
-    echo "$$" > "${pidfile}"
-    if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- initiating; pidfile: ${pidfile}; PID: " $(cat ${pidfile}) &> /dev/stderr; fi
-    temp=$(mktemp -t "${0##*/}-XXXXXX")
-    echo '{"rtsp":'$(find_rtsp)'}' | tee ${temp} | jq -c '.'
-    if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- produced output; output: " $(cat ${temp}) &> /dev/stderr; fi
-    mv -f ${temp} ${1}
-    rm -f ${pidfile}
-  else
-    echo "+++ WARN -- $0 $$ -- currently processing; pidfile: ${pidfile}; PID: " $(cat ${pidfile}) &> /dev/stderr
-  fi
-else
-  echo "*** ERROR -- $0 $$ -- provide file name for output" &> /dev/stderr
+if [ -z "${1:-}" ] || [ ! -e "${1:-}" ]; then
+  echo "*** ERROR -- $0 $$ -- provide fullpath for output file" &>  /dev/stderr
   exit 1
 fi
+
+output=${1:-}
+
+temp=$(mktemp)
+echo '{"rtsp":'$(find_rtsp)'}' | jq -c '.' > ${temp}
+mv -f ${temp} ${output}
+if [ "${DEBUG:-}" = true ]; then echo "--- INFO -- $0 $$ -- output:" $(cat ${output}) &> ${LOG}; fi
